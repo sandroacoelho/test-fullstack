cbea4eb1184d11301c31085350c358a6031e8fb0
sandroacoelho@gmail.com <sandroacoelho@gmail.com>
2017-12-17 00:31:55 -0200

    Adding numbers only directive (validation)

--- test-ui/src/main/resources/static-dev/app/user/directives/user.directives.js
+++ test-ui/src/main/resources/static-dev/app/user/directives/user.directives.js
@@ -19,4 +19,26 @@
         };
     });
 
+
+    module.directive('numbersOnly', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attr, ngModelCtrl) {
+                function fromUser(text) {
+                    if (text) {
+                        var transformedInput = text.replace(/[^0-9]/g, '');
+
+                        if (transformedInput !== text) {
+                            ngModelCtrl.$setViewValue(transformedInput);
+                            ngModelCtrl.$render();
+                        }
+                        return transformedInput;
+                    }
+                    return undefined;
+                }
+                ngModelCtrl.$parsers.push(fromUser);
+            }
+        };
+    });
+
 })(angular.module('user'));
